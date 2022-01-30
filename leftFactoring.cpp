8beb2bd5ff1d68d3a42c633bc731088d158d07cd
Soumyadeep Ghosh <67363979+soumyadepp@users.noreply.github.com>
2022-01-30 18:06:29 +0530

    Update leftFactoring.cpp

--- leftFactoring.cpp
+++ leftFactoring.cpp
@@ -1,92 +1,76 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
-
 int main()
-{
-    string input;
-    getline(cin,input,'\n');
-    vector<string>prods;
-    string temp = "";
-    for(int i = 0 ; i < input.size(); i++){
-      if(input[i] == ' '){
-        prods.push_back(temp);
-        temp = "";
-      }
-      temp += input[i];
+{  string ip,op1,op2,temp;
+    int sizes[10] = {};
+    char c;
+    int n,j,l;
+    cout<<"Enter the Parent Non-Terminal : ";
+    cin>>c;
+    ip.push_back(c);
+    op1 += ip + "\'->";
+    op2 += ip + "\'\'->";;
+    ip += "->";
+    cout<<"Enter the number of productions : ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter Production "<<i+1<<" : ";
+        cin>>temp;
+        sizes[i] = temp.size();
+        ip+=temp;
+        if(i!=n-1)
+            ip += "|";
     }
-    if(temp.size() > 0) prods.push_back(temp);
-    vector<string> prodleft,prodright;
-    for(auto i : prods){
-      string chk = i;
-      string left = "";
-      string right = "";
-      bool flag = false;
-      for(int j = 0 ; j < chk.size(); j++){
-        if(j < chk.size() - 1 && chk[j] == '-' && chl[j + 1] == '>'){
-          flag = true;
-          continue;
+    cout<<"Production Rule : "<<ip<<endl;
+    char x = ip[3];
+    for(int i=0,k=3;i<n;i++)
+    {
+        if(x == ip[k])
+        {
+                if(ip[k+1] == '|')
+                {
+                    op1 += "#";
+                    ip.insert(k+1,1,ip[0]);
+                    ip.insert(k+2,1,'\'');
+                    k+=4;
+                }
+                else
+                {
+                    op1 += "|" + ip.substr(k+1,sizes[i]-1);
+                    ip.erase(k-1,sizes[i]+1);
+                }
         }
-        if(flag == false){
-          leftString += chk[j];
-        }
-        else{
-          rightString += chk[j];
-        }
-      }
-      prodleft.push_back(leftString);
-      prodright.push_back(rightString);
-    }
-    int p = prodleft.size();
-    int j;  
-    int e=1;
-    for(i=0;i<p;++i) {
-        for(j=i+1;j<p;++j) {
-            if(prodleft[j]==prodleft[i]) {
-                int k=0;
-                string com="";
-                while(k<prodright[i].length()&&k<prodright[j].length()&&prodright[i][k]==prodright[j][k]) {
-                    com+=prodright[i][k];
-                    ++k;
-                }
-                if(k==0)
-                    continue;
-                char* buffer;
-                string comleft=prodleft[i];
-                if(k==prodright[i].length()) {
-                    prodleft[i]+=to_string(e);
-                    prodleft[j]+=to_string(e);
-                    prodright[i]="^";
-                    prodright[j]=prodright[j].substr(k,prodright[j].length()-k);
-                }
-                else if(k==prodright[j].length()) {
-                    prodleft[i]+=to_string(e);
-                    prodleft[j]+=to_string(e);
-                    prodright[j]="^";
-                    prodright[i]=prodright[i].substr(k,prodright[i].length()-k);
-                }
-                else {
-                    prodleft[i]+=to_string(e);
-                    prodleft[j]+=to_string(e);
-                    prodright[j]=prodright[j].substr(k,prodright[j].length()-k);
-                    prodright[i]=prodright[i].substr(k,prodright[i].length()-k);
-                }
-                int l;
-                for(l=j+1;l<p;++l) {
-                    if(comleft==prodleft[l]&&com==prodright[l].substr(0,fmin(k,prodright[l].length()))) {
-                        prodleft[l]+=to_string(e);
-                        prodright[l]=prodright[l].substr(k,prodright[l].length()-k);
-                    }
-                }
-                prodleft.push_back(comleft);
-                prodright.push_back(com+prodleft[i]);
-                ++p;
-                ++e;
-            }
+        else
+        {
+            while(ip[k++]!='|');
         }
     }
-    cout<<"\n\nNew productions";
-    for(i=0;i<p;++i) {
-        cout<<"\n"<<prodleft[i]<<"->"<<prodright[i];
-    }
+    char y = op1[6];
+    for(int i=0,k=6;i<n-1;i++)
+    {
+        if(y == op1[k])
+        {
+                if(op1[k+1] == '|')
+                {
+                    op2 += "#";
+                    op1.insert(k+1,1,op1[0]);
+                    op1.insert(k+2,2,'\'');
+                    k+=5;
+                }
+                else
+                {
+                    temp.clear();
+                    for(int s=k+1;s<op1.length();s++)
+                        temp.push_back(op1[s]);
+                    op2 += "|" + temp;
+                    op1.erase(k-1,temp.length()+2);
+                } }}
+    op2.erase(op2.size()-1);
+    cout<<"After Left Factoring : "<<endl;
+    cout<<ip<<endl;
+    cout<<op1<<endl;
+    cout<<op2<<endl;
     return 0;
 }
